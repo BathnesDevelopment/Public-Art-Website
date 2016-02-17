d8febedac707a5510ad39dda233d7f93c805def0
Dave <david_rowe@bathnes.gov.uk>
2016-02-17 11:50:17 +0000

    Setting up basic site

---
+++ scripts/publicart.js
@@ -0,0 +1,43 @@
+﻿////////////////////////////////////////////////////////////
+// Publicart.js: Handles the functions for retrieving public
+// art catalogue data.
+// Created: 17th Feb 2016.
+////////////////////////////////////////////////////////////
+var PublicArt = {
+    // The location of the public art data.
+    datastoreUrl: 'https://',
+    // The fields to return when listing items.
+    // Title, Artist, Date, Type of work
+    listFilter: '',
+
+    /////////////////////////////////////////////////////
+    // Function: getFiltered
+    // Input: callback
+    // Return: the default JSON returned by the datastore
+    /////////////////////////////////////////////////////
+    getFiltered: function (callback) {
+
+        // Get the data from the datastore
+
+
+    },
+
+    /////////////////////////////////////////////////////
+    // Function: getAll
+    // Input: callback
+    // Return: the default JSON returned by the datastore
+    /////////////////////////////////////////////////////
+    getAll: function (callback) {
+
+    },
+
+    /////////////////////////////////////////////////////
+    // Function: getItem
+    // Input: id of the item to return (e.g. BA1)
+    // Return: the default JSON returned by the datastore
+    /////////////////////////////////////////////////////
+    getItem: function (id, callback) {
+
+
+    }
+}
