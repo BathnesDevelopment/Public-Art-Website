5b226fb3bd838b5b10fa1bc5c0d686e307fdd0df
Dave <david_rowe@bathnes.gov.uk>
2016-02-23 15:45:25 +0000

    Working on Shuffle images

--- scripts/site.js
+++ scripts/site.js
@@ -12,12 +12,19 @@ $(function () {
         var dataSet = [];
         $.each(data, function (key, value) {
 
+
+            var altText = '';
+
+            var imgUrl = 'http://www.bathnes.gov.uk/sites/default/files/publicart/thumbnails/' + value.reference + '-a.jpg';
+
             var categoryHtml = '';
+            var categoryArray = '';
             if (value.categories) {
                 var categories = value.categories.split('|');
                 $.each(categories, function (idx, cat) {
                     categoryHtml += '<span class="label label-default">' + cat + '</span> ';
                 });
+                categoryArray = value.categories.split('|').join('","');
             }
 
             var artistHtml = '';
@@ -30,10 +37,10 @@ $(function () {
                 if (value.artist6_name) artistHtml += ', ' + value.artist6_name;
             }
 
-            $('#grid').append('<div class="grid__brick col-xs-6 col-sm-4" data-title="This is a title 1" data-date="2014"><a class="thumbnail" href="#"><img class="img-responsive" src="http://placehold.it/400x300" alt=""></a></div>');
-
-
+            $('#grid').append('<div class="grid__brick col-lg-3 col-md-4 col-xs-6" data-groups=&apos;[' + categoryArray + ']&apos; data-title="' + value.title + '" data-date="' + value.date + '"><div class="thumbnail"><img class="img-responsive" src="' + imgUrl + '" alt="' + altText + '"><div class="wrapper"><div class="caption post-content"><p class="lead">' + value.title + '</p>' + categoryHtml + '</div></div></div></div>');
         });
+
+        $('#grid').append('<div class="col-xs-1 shuffle__sizer"></div>');
 
         // Set up Shuffle on the grid bricks.
         var grid = $('#grid')
@@ -44,32 +51,44 @@ $(function () {
         $('.sort-options').on('change', function () {
             var sort = this.value,
                 opts = {};
-
             // We're given the element wrapped in jQuery
             if (sort === 'date') {
                 opts = {
                     reverse: true,
-                    by: function ($el) {
-                        return $el.data('date');
+                    by: function (el) {
+                        return el.data('date');
                     }
                 };
             } else if (sort === 'title') {
                 opts = {
-                    by: function ($el) {
-                        return $el.data('title').toLowerCase();
+                    by: function (el) {
+                        return el.data('title').toLowerCase();
                     }
                 };
             }
-
-            // Filter elements
             grid.shuffle('sort', opts);
         });
 
-
-
-        $('#grid').shuffle('shuffle', function ($el, shuffle) {
-            return $el.data('title').length < 20;
+        // Search
+        $('.js-shuffle-search').on('keyup change', function () {
+            var val = this.value.toLowerCase();
+            grid.shuffle('shuffle', function (el, shuffle) {
+                // Only search elements in the current group
+                if (shuffle.group !== 'all' && $.inArray(shuffle.group, el.data('groups')) === -1) {
+                    return false;
+                }
+                var text = $.trim(el.find('.lead').text()).toLowerCase();
+                return text.indexOf(val) !== -1;
+            });
         });
 
+        // Filter
+        $('.btn-filter').on('click', function () {
+            var isActive = $(this).hasClass('active');
+            var group = isActive ? 'all' : $(this).data('group');
+            if (!isActive) $('.filter-options .active').removeClass('active');
+            $(this).toggleClass('active');
+            grid.shuffle('shuffle', group);
+        });
     });
 });
